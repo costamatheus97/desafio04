983ec2d7e8397d7e662cbe4625ab5bcb2895ec85
Matheus <matheus.costa@b8one.com>
2020-09-16 22:22:01 -0300

    finished challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,48 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions
   }
 
   public getBalance(): Balance {
-    // TODO
+    const transactions = this.transactions;
+
+    let incomeValue = 0
+    let outcomeValue = 0
+
+    if(transactions.length > 0){
+      if(transactions.length > 1){
+        transactions.map(item => {
+          item.type === 'income' ? incomeValue += item.value : outcomeValue += item.value
+        })
+      } else {
+        transactions[0].type === 'income' ? incomeValue = transactions[0].value : outcomeValue = transactions[0].value
+      }
+
+      const balance = {
+        income: incomeValue,
+        outcome: outcomeValue,
+        total: incomeValue - outcomeValue
+      }
+
+      return balance
+    } else {
+      const balance = {
+        income: incomeValue,
+        outcome: outcomeValue,
+        total: 0
+      }
+
+      return balance
+    }
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({title, value, type}: CreateTransactionDTO): Transaction{
+    const transaction = new Transaction({title, value, type});
+
+    this.transactions.push(transaction);
+
+    return transaction
   }
 }
 
